22591a05c348e96d39940118287b132e2813ea45
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2024-06-19 21:36:32 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,18 +1,18 @@
-# fruit =10
-# fruit1 =20
-# print("The fruit is: ", (fruit+fruit1))
-# s = "Prithwiraj khanki"
-# len_str = len(s)
-# print (len_str)
-# print (s[8])
-# s_new = s.replace("w","f")
-# print (s_new)
-# r = s[4:10]
-# print (r)
-# name = input("write: ")
-# age = input("write: ")
-# print ("hello",name,"!","my age",age,"!")
-a = 0
-while a>=0:
-    a += 1
-    print(a)
+# # fruit =10
+# # fruit1 =20
+# # print("The fruit is: ", (fruit+fruit1))
+# # s = "Prithwiraj khanki"
+# # len_str = len(s)
+# # print (len_str)
+# # print (s[8])
+# # s_new = s.replace("w","f")
+# # print (s_new)
+# # r = s[4:10]
+# # print (r)
+# # name = input("write: ")
+# # age = input("write: ")
+# # print ("hello",name,"!","my age",age,"!")
+# a = 0
+# while a>=0:
+#     a += 1
+#     print(a)
